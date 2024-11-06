fdbe197e2b4fe8ff2ce31a0b5a7a33b64559b5de
Adam Marley <141704661+marleyad@users.noreply.github.com>
2024-11-06 12:22:33 -0500

    Update find-floor.test.js

--- find-floor.test.js
+++ find-floor.test.js
@@ -1,6 +1,5 @@
 const findFloor = require("./find-floor")
 
-
 describe("#findFloor", function(){
   it("returns the floor or -1", function(){
     expect(findFloor([1, 2, 8, 10, 10, 12, 19], 5)).toBe(2) // 2
